533c0fcfa24c8e670881dec70bfe1846d13c267e
isandeepkadam <sandeepkadam.dev@gmail.com>
2025-01-12 22:22:26 +0530

    feat: puck editor UI

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -41,8 +41,9 @@ export default function Editor() {
                   localStorage.setItem('puck', JSON.stringify(appState.data))
                   // save(appState.data);
                 }}
+                style={{ color: "white"}}
               >
-                Save something
+                Save
               </button>
             </>
           );
